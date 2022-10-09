7844bfef0a4b9e1e9a0b97a8092037e807cbff32
mos <77763580+TheMosKau@users.noreply.github.com>
2022-10-09 23:49:20 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,8 @@
 
 git submodule update --recursive --init && ./scripts/applyPatches.sh
 if [ "$1" == "--jar" ]; then
+    git config --global user.email "github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>"
+    git config --global user.name "github-actions"
     pushd FlameCord-Proxy
     mvn clean package -f pom.xml
 fi
